95e7cc5b65aa8f25d6dde3e14d4aae06d865d0bd
amandasimonds <amandasimonds9@gmail.com>
2021-02-16 08:42:22 -0700

    used methods for ndstyle and ngclass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,14 @@
+import { NgStyle } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online{
+      color: white;
+    }
+  `]
 })
 
 export class ServerComponent {
@@ -10,8 +16,15 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = "offline";
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline' ;
+  }
+
   getServerStatus () {
     return this.serverStatus;
   }
 
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red' ;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created";
-  serverName = "testserver";
+  serverName = "no name";
+  serverCreated = false;
+  servers = ['Test server', 'Test server 2']
 
   constructor() {
     setTimeout(() => {
@@ -20,11 +22,16 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created!"
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = "Server " + this.serverName + " was created!";
   }
 
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
   };
 
+  onDeleteServer(){
+    this.servers.splice(1, 1);
+  }
 }
